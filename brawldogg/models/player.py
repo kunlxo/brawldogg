9c52abe1324ab471e2d551dee59fba878550d27d
kunlxo <kunlxouhh@gmail.com>
2025-11-27 03:09:48 +0300

    add brawler id to winstreak

--- brawldogg/models/player.py
+++ brawldogg/models/player.py
@@ -12,6 +12,11 @@ class PlayerIcon(BaseModel):
     id: int
 
 
+class WinStreak(BaseModel):
+    winstreak: int = 0
+    brawler_id: int | None = None
+
+
 class Player(BaseModel):
     tag: str
     name: str
@@ -32,16 +37,32 @@ class Player(BaseModel):
     club: PlayerClub | None
     brawlers: list[BrawlerStat] = Field(default_factory=list)
 
-    max_winstreak: int = 0
-    current_winstreak: int = 0
+    max_winstreak: WinStreak
+    current_winstreak: WinStreak
 
     @model_validator(mode="after")
     def compute_winstreaks(self):
         max_winstreak = 0
+        max_winstreak_id = None
+
         current_winstreak = 0
+        current_winstreak_id = None
+
         for b in self.brawlers:
             max_winstreak = max(max_winstreak, b.max_win_streak)
             current_winstreak = max(current_winstreak, b.current_win_streak)
-        self.max_winstreak = max_winstreak
-        self.current_winstreak = current_winstreak
+            if b.max_win_streak > max_winstreak:
+                max_winstreak = b.max_win_streak
+                max_winstreak_id = b.id
+
+            if b.current_win_streak > current_winstreak:
+                current_winstreak = b.current_win_streak
+                current_winstreak_id = b.id
+
+        self.max_winstreak = WinStreak(
+            winstreak=max_winstreak, brawler_id=max_winstreak_id
+        )
+        self.current_winstreak = WinStreak(
+            winstreak=current_winstreak, brawler_id=current_winstreak_id
+        )
         return self
