e3b8b1bf77ca51d5940e82b0748741c35c2e112a
kunlxo <kunlxouhh@gmail.com>
2025-11-27 03:11:06 +0300

    add import for winstreak

--- brawldogg/models/__init__.py
+++ brawldogg/models/__init__.py
@@ -3,13 +3,14 @@ from .brawler import Brawler, BrawlerStat, Gadget, StarPower
 from .club import Club, ClubMember, ClubName
 from .events import Event, EventEntry, GameMode
 from .paging import PagingResponse
-from .player import Player, PlayerClub, PlayerIcon
+from .player import Player, PlayerClub, PlayerIcon, WinStreak
 from .rankings import ClubRanking, PlayerRanking
 
 __all__ = [
     "Player",
     "PlayerIcon",
     "PlayerClub",
+    "WinStreak",
     "BattleLogEntry",
     "Battle",
     "BattlePlayer",
